516720e1c86e2e8f4952624dbc35ddf5e743e445
k1enn <dinhtrungkien05@gmail.com>
2024-10-24 10:54:36 +0700

    update something idk

---
+++ Expense-Tracker-App/Expense-Tracker-App/Models/DBcontex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Expense_Tracker_App.Models
+{
+    public class DBcontex
+    {
+    }
+}
---
+++ Expense-Tracker-App/Expense-Tracker-App/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Expense_Tracker_App.Models
+{
+    public class User
+    {
+        int ID { get; set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
